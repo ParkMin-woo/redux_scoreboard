50236594949c653c35f1a58c99c43833f9444901
Park Minwoo <lwmwpark3523@gmail.com>
2019-10-12 14:51:27 +0900

    Routing 구성

---
+++ src/pages/Root.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Home} from "./Home";
+import {Heroes} from "./Heroes";
+import {Scoreboard} from "./Scoreboard";
+
+// 최초 loading되는 Component
+export class Root extends React.Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<p>공통메뉴영역</p>
+				{/* Path와 반드시 일치하는 것이 아니라 시작하는 URL로 Mapping하게 된다. */}
+				<Switch>
+					<Route exact path = "/" component = {Home}></Route>
+					<Route path = "/heroes" component = {Heroes}></Route>
+					<Route path = "/scoreboard" component = {Scoreboard}></Route>
+				</Switch>
+			</BrowserRouter>
+		);
+	}
+}
