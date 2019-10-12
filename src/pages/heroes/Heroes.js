eaf6e4ecf0b3ef2f86c2b4d384760a6a6c07af00
Park Minwoo <lwmwpark3523@gmail.com>
2019-10-12 18:14:20 +0900

    image 안보이는거 디폴트줘서 보이게

--- src/pages/heroes/Heroes.js
+++ src/pages/heroes/Heroes.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import axios from 'axios';
+import api from '../../utils/api';
+
+import './Heroes.scss';
 
 export class Heroes extends React.Component {
+
+	// constructor() {
+	// 	super();
+	// 	this.state = [];
+	// }
+
+	state = {
+		heroes: []
+	};
 
 	componentDidMount() {
 		// body.data에 원하는 결과값이 있다.
@@ -13,15 +24,28 @@ export class Heroes extends React.Component {
 	// await 뒤에는 반드시 Promise 객체가 와야 한다.
 	// await는 결과가 올때까지 기다렸다가 결과값을 리턴한다.
 	async getHeroes() {
-		const body = await axios.get('http://ec2-15-164-134-124.ap-northeast-2.compute.amazonaws.com:8000/api/user/heroes?start_index=11&page_size=20');
+		const body = await api.get('/api/user/heroes?start_index=1&page_size=5');
 		console.log("body : " , body);
+
+		this.setState({heroes : body.data.data});
 	}
 
 	render() {
 		return (
-			<div>
-				Heroes WORKS!!
-			</div>
+			<ul>
+				{this.state.heroes.map((hero) => {
+					return (
+					<li key={hero.id} className="row align-items-center m-0">
+						<div className="col-1 py-2">
+						{/*<img src={hero.photo} alt={hero.name} className="img-fluid rounded-circle" />*/}
+						<img src={hero.photo ? hero.photo : process.env.PUBLIC_URL + '/images/face-24px.svg'} alt={hero.name}
+									 className="img-fluid rounded-circle" style={{width: '100%'}} />
+						</div>
+					<span className="col">{hero.name}</span>
+					</li>
+					);
+				})}
+			</ul>
 		);
 	}
 }
